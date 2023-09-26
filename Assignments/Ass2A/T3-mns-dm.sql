9bbc8fddfaa12516ea3bb40a04a534c1f110b903
zliu0207 <zliu0207@student.monash.edu>
2023-09-26 18:40:07 +1000

    t3 and t4 done. need further adjustment.

--- Assignments/Ass2A/T3-mns-dm.sql
+++ Assignments/Ass2A/T3-mns-dm.sql
@@ -15,16 +15,232 @@
 
 
 --3(a)
+DROP SEQUENCE ec_seq;
+
+DROP SEQUENCE patient_seq;
+
+DROP SEQUENCE appt_seq;
+
+CREATE SEQUENCE ec_seq START WITH 100 INCREMENT BY 5;
+
+CREATE SEQUENCE patient_seq START WITH 100 INCREMENT BY 5;
+
+CREATE SEQUENCE appt_seq START WITH 100 INCREMENT BY 5;
 
 
 --3(b)
+INSERT INTO emergency_contact VALUES (
+    ec_seq.NEXTVAL,
+    'Jonathan',
+    'Robey',
+    '0412523122'
+);
 
+INSERT INTO patient VALUES (
+    patient_seq.NEXTVAL,
+    'Laura',
+    'Robey',
+    '12 moverly rd',
+    'Melbourne',
+    'VIC',
+    '3000',
+    TO_DATE('05-May-2016', 'dd-Mon-yyyy'),
+    '0412523122',
+    'laurarobey@gmail.com',
+    (
+        SELECT
+            ec_id
+        FROM
+            emergency_contact
+        WHERE
+            ec_phone = '0412523122'
+    )
+);
+
+INSERT INTO appointment VALUES (
+    appt_seq.NEXTVAL,
+    TO_DATE('04-Sep-2023 15:30', 'dd-Mon-yyyy HH24:MI'),
+    1,
+    'S',
+    (
+        SELECT
+            patient_no
+        FROM
+            patient
+        WHERE
+                upper(patient_fname) = upper('Laura')
+            AND upper(patient_lname) = upper('Robey')
+    ),
+    (
+        SELECT
+            provider_code
+        FROM
+            provider
+        WHERE
+                upper(provider_title) = upper('Dr')
+            AND upper(provider_fname) = upper('Bruce')
+            AND upper(provider_lname) = upper('STRIPLIN')
+    ),
+    6,
+    NULL
+);
+
+INSERT INTO patient VALUES (
+    patient_seq.NEXTVAL,
+    'Lachlan',
+    'Robey',
+    '12 moverly rd',
+    'Melbourne',
+    'VIC',
+    '3000',
+    TO_DATE('21-Sep-2017', 'dd-Mon-yyyy'),
+    '0412523122',
+    'lachlanrobey@gmail.com',
+    (
+        SELECT
+            ec_id
+        FROM
+            emergency_contact
+        WHERE
+            ec_phone = '0412523122'
+    )
+);
+
+INSERT INTO appointment VALUES (
+    appt_seq.NEXTVAL,
+    TO_DATE('04-Sep-2023 16:00', 'dd-Mon-yyyy HH24:MI'),
+    2,
+    'S',
+    (
+        SELECT
+            patient_no
+        FROM
+            patient
+        WHERE
+                upper(patient_fname) = upper('Lachlan')
+            AND upper(patient_lname) = upper('Robey')
+    ),
+    (
+        SELECT
+            provider_code
+        FROM
+            provider
+        WHERE
+                upper(provider_title) = upper('Dr')
+            AND upper(provider_fname) = upper('Bruce')
+            AND upper(provider_lname) = upper('STRIPLIN')
+    ),
+    6,
+    NULL
+);
+
+COMMIT;
 
 --3(c)
+INSERT INTO appointment VALUES (
+    appt_seq.NEXTVAL,
+    TO_DATE('14-Sep-2023 16:00', 'dd-Mon-yyyy HH24:MI'),
+    2,
+    'T',
+    (
+        SELECT
+            patient_no
+        FROM
+            patient
+        WHERE
+                upper(patient_fname) = upper('Lachlan')
+            AND upper(patient_lname) = upper('Robey')
+    ),
+    (
+        SELECT
+            provider_code
+        FROM
+            provider
+        WHERE
+                upper(provider_title) = upper('Dr')
+            AND upper(provider_fname) = upper('Bruce')
+            AND upper(provider_lname) = upper('STRIPLIN')
+    ),
+    14,
+    (
+        SELECT
+            appt_no
+        FROM
+            appointment
+        WHERE
+                appt_datetime = TO_DATE('04-Sep-2023 16:00', 'dd-Mon-yyyy HH24:MI')
+            AND appt_roomno = 2
+            AND patient_no = (
+                SELECT
+                    patient_no
+                FROM
+                    patient
+                WHERE
+                        upper(patient_fname) = upper('Lachlan')
+                    AND upper(patient_lname) = upper('Robey')
+            )
+            AND provider_code = (
+                SELECT
+                    provider_code
+                FROM
+                    provider
+                WHERE
+                        upper(provider_title) = upper('Dr')
+                    AND upper(provider_fname) = upper('Bruce')
+                    AND upper(provider_lname) = upper('STRIPLIN')
+            )
+            AND appt_prior_apptno IS NULL
+    )
+);
+
+COMMIT;
 
 
 --3(d)
 
+UPDATE appointment
+SET
+    appt_datetime = TO_DATE('18-Sep-2023 16:00', 'dd-Mon-yyyy HH24:MI')
+WHERE
+        appt_datetime = TO_DATE('14-Sep-2023 16:00', 'dd-Mon-yyyy HH24:MI')
+    AND appt_roomno = 2
+    AND patient_no = (
+        SELECT
+            patient_no
+        FROM
+            patient
+        WHERE
+                upper(patient_fname) = upper('Lachlan')
+            AND upper(patient_lname) = upper('Robey')
+    )
+    AND provider_code = (
+        SELECT
+            provider_code
+        FROM
+            provider
+        WHERE
+                upper(provider_title) = upper('Dr')
+            AND upper(provider_fname) = upper('Bruce')
+            AND upper(provider_lname) = upper('STRIPLIN')
+    );
+
+COMMIT;
+
 
 --3(e)
+DELETE FROM appointment
+WHERE
+    appt_datetime BETWEEN TO_DATE('15-Sep-2023', 'dd-Mon-yyyy') AND TO_DATE('22-Sep-2023'
+    , 'dd-Mon-yyyy')
+    AND provider_code = (
+        SELECT
+            provider_code
+        FROM
+            provider
+        WHERE
+                upper(provider_title) = upper('Dr')
+            AND upper(provider_fname) = upper('Bruce')
+            AND upper(provider_lname) = upper('STRIPLIN')
+    );
 
+COMMIT;
--- Assignments/Ass2A/T4-mns-alter.sql
+++ Assignments/Ass2A/T4-mns-alter.sql
@@ -1,10 +1,10 @@
 --*****PLEASE ENTER YOUR DETAILS BELOW*****
 --T4-mns-alter.sql
 
---Student ID:
---Student Name:
---Unit Code:
---Applied Class No:
+--Student ID: 25348914
+--Student Name: Zecan Liu
+--Unit Code: FIT2094
+--Applied Class No: A04
 
 /* Comments for your marker:
 
@@ -14,10 +14,112 @@
 */
 
 --4(a)
+ALTER TABLE patient ADD (
+    patient_appt_amount NUMBER(3, 0)
+);
+
+COMMENT ON COLUMN patient.patient_appt_amount IS
+    'total number of appointments for this patient in the system';
+
+UPDATE patient p
+SET
+    patient_appt_amount = (
+        SELECT
+            COUNT(*)
+        FROM
+            appointment a
+        WHERE
+            a.patient_no = p.patient_no
+    );
+
+desc patient;
 
 
 --4(b)
+DROP TABLE patient_ec_record CASCADE CONSTRAINTS PURGE;
+
+CREATE TABLE patient_ec_record (
+    patient_no NUMBER(4) NOT NULL,
+    ec_id      NUMBER(4) NOT NULL
+);
+
+COMMENT ON COLUMN patient_ec_record.patient_no IS
+    'patient id number';
+
+COMMENT ON COLUMN patient_ec_record.ec_id IS
+    'emergency contact id number';
+
+ALTER TABLE patient_ec_record ADD CONSTRAINT p_ec_pk PRIMARY KEY ( patient_no,
+                                                                   ec_id );
+
+ALTER TABLE patient_ec_record
+    ADD CONSTRAINT patient_pec_fk FOREIGN KEY ( patient_no )
+        REFERENCES patient ( patient_no );
+
+ALTER TABLE patient_ec_record
+    ADD CONSTRAINT ec_pec_fk FOREIGN KEY ( ec_id )
+        REFERENCES emergency_contact ( ec_id );
+
+desc patient_ec_record;
 
 
 --4(c)
+DROP TABLE training_record CASCADE CONSTRAINTS PURGE;
 
+DROP TABLE training_course CASCADE CONSTRAINTS PURGE;
+
+CREATE TABLE training_course (
+    training_course_id NUMBER(3) NOT NULL,
+    nurse_no           NUMBER(3) NOT NULL,
+    tc_starttime       DATE NOT NULL,
+    tc_endtime         DATE NOT NULL,
+    tc_description     VARCHAR2(100)
+);
+
+COMMENT ON COLUMN training_course.nurse_no IS
+    'trainer nurse id';
+
+COMMENT ON COLUMN training_course.training_course_id IS
+    'training course id';
+
+COMMENT ON COLUMN training_course.tc_starttime IS
+    'training course start datetime';
+
+COMMENT ON COLUMN training_course.tc_endtime IS
+    'training course end datetime';
+
+COMMENT ON COLUMN training_course.tc_description IS
+    'training course description';
+
+ALTER TABLE training_course ADD CONSTRAINT tc_pk PRIMARY KEY ( training_course_id );
+
+ALTER TABLE training_course
+    ADD CONSTRAINT uq_tc UNIQUE ( nurse_no,
+                                  tc_starttime,
+                                  tc_endtime );
+
+CREATE TABLE training_record (
+    nurse_no           NUMBER(3) NOT NULL,
+    training_course_id NUMBER(3) NOT NULL
+);
+
+COMMENT ON COLUMN training_record.nurse_no IS
+    'trainee nurse id';
+
+COMMENT ON COLUMN training_record.training_course_id IS
+    'training course id';
+
+ALTER TABLE training_record ADD CONSTRAINT tr_pk PRIMARY KEY ( nurse_no,
+                                                               training_course_id );
+
+ALTER TABLE training_course
+    ADD CONSTRAINT nurse_tc_fk FOREIGN KEY ( nurse_no )
+        REFERENCES nurse ( nurse_no );
+
+ALTER TABLE training_record
+    ADD CONSTRAINT nurse_tr_fk FOREIGN KEY ( nurse_no )
+        REFERENCES nurse ( nurse_no );
+
+ALTER TABLE training_record
+    ADD CONSTRAINT course_tr_fk FOREIGN KEY ( training_course_id )
+        REFERENCES training_course ( training_course_id );
